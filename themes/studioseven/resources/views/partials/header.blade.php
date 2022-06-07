5e5c973a3c9432285e7085ec344eac0f9502f0a7
Matth-Ben <matthias.benoit@outlook.fr>
2022-06-07 02:09:04 +0200

    ✨ [HEADER] Template + skin

--- themes/studioseven/resources/views/partials/header.blade.php
+++ themes/studioseven/resources/views/partials/header.blade.php
@@ -1,5 +1,22 @@
 <header class="header">
   <div class="container-fluid">
-    <div class="row"></div>
+    <div class="row justify-content-between">
+      <div class="col">
+        <a class="header__sitename" href="{{ get_site_url() }}">
+          {{ get_bloginfo() }}
+        </a>
+      </div>
+      <div class="col">Web Developper<br>Portfolio 2022</div>
+      <div class="col">
+        <div class="button">
+          <a href="#" class="button__content">
+            <div class="button__content-wrapper">
+              <div class="button__content-text">Let's talk</div>
+              <div class="button__content-hover">Let's talk</div>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
   </div>
 </header>
